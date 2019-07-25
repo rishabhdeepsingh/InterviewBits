959b803f8f7f86a4dd15a5c4f6aee12a3a0c13d3
RishabhDeep Singh <rishabhdeepsingh98@gmail.com>
2019-07-25 17:41:13 +0530

    Create sum-of-fibonacci-numbers.cpp

---
+++ Graph/sum-of-fibonacci-numbers.cpp
@@ -0,0 +1,20 @@
+int Solution::fibsum(int n) {
+    vector<int> fib;
+    fib.push_back(1);
+    fib.push_back(1);      // now u have 1 and 1 at v[0] and v[1]
+    for (int i = 2; fib[i - 1] < n; i++) {
+        int val = fib[i - 1] + fib[i - 2];
+        fib.push_back(val);
+    }
+    int ans = 0;
+    int size = fib.size();
+    size--;
+    while (n > 0) {
+        while (fib[size] > n) {        // find the largest fibonaci number < n
+            size--;
+        }                         // now decraese the largest fibonaci number from n
+        n = n - fib[size];
+        ans++;
+    }
+    return ans;
+}
