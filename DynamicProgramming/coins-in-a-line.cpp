035608d66a1ea9e449784df12c8ea920fb65c0ec
RishabhDeep Singh <rishabhdeepsingh98@gmail.com>
2019-07-25 17:36:43 +0530

    Update coins-in-a-line.cpp

--- DynamicProgramming/coins-in-a-line.cpp
+++ DynamicProgramming/coins-in-a-line.cpp
@@ -1,16 +1,17 @@
-int dp[1002][1002][2];
-vector<int> v;
-int solve(int i, int j, int s) {
-    if (i > j)return 0;
-    if (dp[i][j][s])return dp[i][j][s];
-    if (!s) {
-        return dp[i][j][s] = max(v[j] + solve(i, j - 1, 1), v[i] + solve(i + 1, j, 1));
-    } else {
-        return dp[i][j][s] = min(solve(i, j - 1, 0), solve(i + 1, j, 0));
-    }
-}
 int Solution::maxcoin(vector<int>& A) {
-    memset(dp, 0, sizeof(dp));
-    v = A;
-    return solve(0, A.size() - 1, 0);
+    int n = A.size();
+    int table[2][n];
+    memset(table, 0, sizeof(table));
+    for (int sz = 2; sz <= n; sz += 2) {
+        for (int i = 0; i < n - sz + 1; i++) {
+            int j = i + sz - 1;
+            int temp1 = A[i] + min(table[0][i + 1], table[0][i + 2]);
+            int temp2 = A[j] + min(table[0][i + 1], table[0][i]);
+            table[1][i] = max(temp1, temp2);
+        }
+        for (int i = 0; i < n - sz + 1; i++) {
+            table[0][i] = table[1][i];
+        }
+    }
+    return table[1][0];
 }
