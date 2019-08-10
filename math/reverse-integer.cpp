d0c34905cd493a616294e39e1362511562d7c4d0
RishabhDeep Singh <rishabhdeepsingh98@gmail.com>
2019-08-10 18:12:27 +0530

    Create reverse-integer.cpp

---
+++ math/reverse-integer.cpp
@@ -0,0 +1,15 @@
+int Solution::reverse(int A) {
+    bool neg = A < 0;
+    if(neg){
+        A *= -1;
+    }
+    int res = 0;
+    while(A){
+        if(res > INT_MAX / 10) return 0;
+        res *= 10;
+        res += A%10;
+        A /= 10;
+    }
+    if(neg) res *= -1;
+    return res;
+}
