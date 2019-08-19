ffb396728fd81e7bd23187ddb9821fc216f7ad4c
RishabhDeep Singh <rishabhdeepsingh98@gmail.com>
2019-08-19 18:47:18 +0530

    Create square-root-of-integer.cpp

---
+++ BinarySearch/square-root-of-integer.cpp
@@ -0,0 +1,15 @@
+int Solution::sqrt(int A) {
+    if (A < 2) return A;
+    int start = 0, end = A;
+    int ans;
+    while(start<=end){
+        int mid = start + (end - start)/2;
+        if (mid <= A/mid) {
+            start = mid + 1;
+            ans = mid;
+        }else{ 
+            end = mid - 1;
+        }
+    }
+    return ans;
+}
