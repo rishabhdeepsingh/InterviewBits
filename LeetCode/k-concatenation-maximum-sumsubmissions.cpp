7a2b57436728464db0afe2f8b1485db649102333
RishabhDeep Singh <rishabhdeepsingh98@gmail.com>
2019-09-22 23:15:02 +0530

    Update k-concatenation-maximum-sumsubmissions.cpp

--- LeetCode/k-concatenation-maximum-sumsubmissions.cpp
+++ LeetCode/k-concatenation-maximum-sumsubmissions.cpp
@@ -2,25 +2,14 @@ class Solution {
 public:
     const int MOD = (int) 1e9 + 7;
     typedef long long ll;
-    int kConcatenationMaxSum(vector<int>& a, int k) {
-        int n = a.size();
-        ll sum = accumulate(a.begin(), a.end(), 0ll);
-        ll res = 0, acc = 0;
-        for (int x: a) {
-            acc += x;
-            if (acc < 0) acc = 0;
-            res = max(res, acc);
+    int kConcatenationMaxSum(vector<int>& arr, int k) {
+        ll n = arr.size();
+        ll sum = arr[0], mx = max(0, arr[0]);
+        ll total = accumulate(arr.begin(), arr.end(), 0);
+        for (int i = 1; i < n * min(k, 2); i++) {
+            sum = max((ll) arr[i % n], sum + arr[i % n]);
+            mx = max(mx, sum);
         }
-        if (k >= 2) {
-            vector<int> l = a, r = a;
-            for (int i = 1; i < n; ++i) l[i] += l[i - 1];
-            for (int i = n - 2; i >= 0; --i) r[i] += r[i + 1];
-            ll curr = 0;
-            curr += max(0, *max_element(l.begin(), l.end()));
-            curr += max(0, *max_element(r.begin(), r.end()));
-            curr += max(0ll, sum * (k - 2));
-            res = max(res, curr);
-        }
-        return res % MOD;
+        return (max( 0ll, total * (k - 2)) + mx) % MOD;
     }
 };
