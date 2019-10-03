f48edb0f7effe1a43a3c2913b10098ffe453c0d3
RishabhDeep Singh <rishabhdeepsingh98@gmail.com>
2019-10-03 20:42:46 +0530

    Update minimum-moves-to-reach-target-with-rotations.cpp

--- LeetCode/minimum-moves-to-reach-target-with-rotations.cpp
+++ LeetCode/minimum-moves-to-reach-target-with-rotations.cpp
@@ -31,22 +31,22 @@ class Solution {
             auto top = q.front();
             q.pop();
             int r = top.first.first, c = top.first.second, dir = top.second;
-            int distance = dist[r][c][dir];
+            int distance = dist[r][c][dir] + 1;
             int r2 = dir == HOR ? r : r + 1;
             int c2 = dir == HOR ? c + 1 : c;
 
             if (valid(r, c + 1) && valid(r2, c2 + 1) && !grid[r][c + 1] && !grid[r2][c2 + 1])
-                bfs_check(r, c + 1, dir, distance + 1);
+                bfs_check(r, c + 1, dir, distance);
 
             if (valid(r + 1, c) && valid(r2 + 1, c2) && !grid[r + 1][c] && !grid[r2 + 1][c2])
-                bfs_check(r + 1, c, dir, distance + 1);
+                bfs_check(r + 1, c, dir, distance);
 
             if (dir == HOR) {
                 if (valid(r + 1, c) && valid(r + 1, c + 1) && !grid[r + 1][c] && !grid[r + 1][c + 1])
-                    bfs_check(r, c, VER, distance + 1);
+                    bfs_check(r, c, VER, distance);
             } else {
                 if (valid(r, c + 1) && valid(r + 1, c + 1) && !grid[r][c + 1] && !grid[r + 1][c + 1])
-                    bfs_check(r, c, HOR, distance + 1);
+                    bfs_check(r, c, HOR, distance);
             }
         }
         int answer = dist[R - 1][C - 2][HOR];
