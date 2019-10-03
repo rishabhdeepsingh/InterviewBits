0318e8fd09c05317e2f77206b9da72699694744e
RishabhDeep Singh <rishabhdeepsingh98@gmail.com>
2019-10-03 20:41:01 +0530

    Update minimum-moves-to-reach-target-with-rotations.cpp

--- LeetCode/minimum-moves-to-reach-target-with-rotations.cpp
+++ LeetCode/minimum-moves-to-reach-target-with-rotations.cpp
@@ -10,9 +10,9 @@ bool valid(int r, int c) {
     return 0 <= r && r < R && 0 <= c && c < C;
 }
 
-void bfs_check(int r, int c, int dir, int current_dist) {
-    if (current_dist < dist[r][c][dir]) {
-        dist[r][c][dir] = current_dist;
+void bfs_check(int r, int c, int dir, int currDist) {
+    if (currDist < dist[r][c][dir]) {
+        dist[r][c][dir] = currDist;
         q.push({{r, c}, dir});
     }
 }
@@ -31,22 +31,22 @@ class Solution {
             auto top = q.front();
             q.pop();
             int r = top.first.first, c = top.first.second, dir = top.second;
-            int my_dist = dist[r][c][dir];
+            int distance = dist[r][c][dir];
             int r2 = dir == HOR ? r : r + 1;
             int c2 = dir == HOR ? c + 1 : c;
 
             if (valid(r, c + 1) && valid(r2, c2 + 1) && !grid[r][c + 1] && !grid[r2][c2 + 1])
-                bfs_check(r, c + 1, dir, my_dist + 1);
+                bfs_check(r, c + 1, dir, distance + 1);
 
             if (valid(r + 1, c) && valid(r2 + 1, c2) && !grid[r + 1][c] && !grid[r2 + 1][c2])
-                bfs_check(r + 1, c, dir, my_dist + 1);
+                bfs_check(r + 1, c, dir, distance + 1);
 
             if (dir == HOR) {
                 if (valid(r + 1, c) && valid(r + 1, c + 1) && !grid[r + 1][c] && !grid[r + 1][c + 1])
-                    bfs_check(r, c, VER, my_dist + 1);
+                    bfs_check(r, c, VER, distance + 1);
             } else {
                 if (valid(r, c + 1) && valid(r + 1, c + 1) && !grid[r][c + 1] && !grid[r + 1][c + 1])
-                    bfs_check(r, c, HOR, my_dist + 1);
+                    bfs_check(r, c, HOR, distance + 1);
             }
         }
         int answer = dist[R - 1][C - 2][HOR];
